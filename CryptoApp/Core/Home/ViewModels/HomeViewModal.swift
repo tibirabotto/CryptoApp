97eaf7db1c88373fc273bd426658dc940b299379
Tibirica Neto <tibirabotto@Tibiricas-MacBook-Pro.local>
2022-11-08 11:11:08 -0500

    Download coins using a View Model and Data Service class

--- CryptoApp/Core/Home/ViewModels/HomeViewModal.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModal.swift
@@ -6,16 +6,27 @@
 //
 
 import Foundation
-
+import Combine
 class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [CoinModel] = []
     @Published var portolioCoins: [CoinModel] = []
     
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portolioCoins.append(DeveloperPreview.instance.coin)
-        })
+        addSubScribers()
     }
+    
+    
+    func addSubScribers() {
+        dataService.$allCoins
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+    }
+    
 }
