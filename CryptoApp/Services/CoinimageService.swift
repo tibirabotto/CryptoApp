b193b03922f011ec35b68c62c1629a9b35ae5294
Tibira Botto <tibira.botto@gmail.com>
2022-11-08 17:52:49 -0500

    Saving images to the File Manager

--- CryptoApp/Services/CoinimageService.swift
+++ CryptoApp/Services/CoinimageService.swift
@@ -14,13 +14,26 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+        } else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
+        
         guard let url = URL(string: coin.image) else {return}
         
         imageSubscription = NetworkingManager.download(url: url)
@@ -28,8 +41,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletation, receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadCoinImage = returnedImage else {return}
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadCoinImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
---
+++ CryptoApp/Ultilities/LocalFileManager.swift
@@ -0,0 +1,60 @@
+//
+//  LocalFileManager.swift
+//  CryptoApp
+//
+//  Created by Tibirica Neto on 2022-11-08.
+//
+
+import Foundation
+import SwiftUI
+
+class LocalFileManager {
+    
+    static let instance = LocalFileManager()
+    private init() {}
+    
+    func saveImage(image: UIImage, imageName: String, folderName: String) {
+        
+        createFolderIfNeeded(folderName: folderName)
+        
+        guard let data = image.pngData(),
+              let url = getURLForImage(imageName: imageName, folderName: folderName)
+        else {return}
+        
+        do {
+            try data.write(to: url)
+        } catch let error {
+            print("ERROR saving image. ImageName: \(imageName). \(error)")
+        }
+    }
+    
+    func getImage(imageName: String, folderName: String) -> UIImage? {
+        guard let url = getURLForImage(imageName: imageName, folderName: folderName),
+              FileManager.default.fileExists(atPath: url.path) else {
+              return nil
+        }
+        
+        return UIImage(contentsOfFile: url.path)
+    }
+    
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getURLForFolder(forlderName: folderName) else {return}
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
+            } catch let error {
+                print("ERROR creating directory. FolderName: \(folderName). \(error)")
+            }
+        }
+    }
+    
+    private func getURLForFolder(forlderName: String) -> URL? {
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {return nil}
+        return url.appendingPathComponent(forlderName)
+    }
+    
+    private func getURLForImage(imageName: String, folderName: String) -> URL? {
+        guard let forlderURL = getURLForFolder(forlderName: imageName) else {return nil}
+        return forlderURL.appendingPathComponent(imageName + ".png")
+    }
+}
