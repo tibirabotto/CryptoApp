ee72ff8372f4d4736de74213c026662744a9bfe3
Tibira Botto <tibira.botto@gmail.com>
2022-11-10 09:23:35 -0500

    Filtering data based on search bar text using Combine

--- CryptoApp/Core/Home/ViewModels/HomeViewModal.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModal.swift
@@ -11,6 +11,7 @@ class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [CoinModel] = []
     @Published var portolioCoins: [CoinModel] = []
+    
     @Published var searchText: String = ""
     
     
@@ -24,11 +25,30 @@ class HomeViewModel: ObservableObject {
     
     
     func addSubScribers() {
-        dataService.$allCoins
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowerCasedText = text.lowercased()
+        
+        return coins.filter { coin -> Bool in
+            return  coin.name.lowercased().contains(lowerCasedText) ||
+                    coin.symbol.lowercased().contains(lowerCasedText) ||
+                    coin.id.lowercased().contains(lowerCasedText)
+        }
     }
     
 }
